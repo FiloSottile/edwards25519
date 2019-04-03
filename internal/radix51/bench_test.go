05f41072ee219f958c94a22bdb2f56b49f902b5c
Filippo Valsorda <valsorda@google.com>
2019-04-03 13:00:11 -0400

    internal/radix51: add benchmarks

---
+++ internal/radix51/bench_test.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2019 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package radix51_test
+
+import (
+	"testing"
+
+	"github.com/gtank/ristretto255/internal/radix51"
+)
+
+func BenchmarkAdd(b *testing.B) {
+	var x, y radix51.FieldElement
+	x.One()
+	y.Add(radix51.One, radix51.One)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x.Add(&x, &y)
+	}
+}
+
+func BenchmarkMul(b *testing.B) {
+	var x, y radix51.FieldElement
+	x.One()
+	y.Add(radix51.One, radix51.One)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		x.Mul(&x, &y)
+	}
+}
