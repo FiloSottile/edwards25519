b695f6b6f8ad8aeda0b9f2e97b606f426ba06167
Henry de Valence <hdevalence@hdevalence.ca>
2019-05-08 21:25:55 -0700

    internal/ed25519: move basepoint constant & correct it
    The new values are extracted from the dalek test vectors.

--- internal/edwards25519/tables_test.go
+++ internal/edwards25519/tables_test.go
@@ -1,17 +1,7 @@
 package edwards25519
 
 import (
-	"github.com/gtank/ristretto255/internal/radix51"
 	"testing"
-)
-
-var (
-	B = ProjP3{
-		X: radix51.FieldElement([5]uint64{426475514619346, 2063872706840040, 14628272888959, 107677749330612, 288339085807592}),
-		Y: radix51.FieldElement([5]uint64{1934594822876571, 2049809580636559, 1991994783322914, 1758681962032007, 380046701118659}),
-		Z: radix51.FieldElement([5]uint64{1, 0, 0, 0, 0}),
-		T: radix51.FieldElement([5]uint64{410445769351754, 2235400917701188, 1495825632738689, 1351628537510093, 430502003771208}),
-	}
 )
 
 func TestProjLookupTable(t *testing.T) {
