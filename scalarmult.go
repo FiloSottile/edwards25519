1c833da9b1cc73aab160f10ca13fa987c09d46f6
Filippo Valsorda <hi@filippo.io>
2020-11-23 14:21:07 +0100

    edwards25519: clarify ScalarBaseMult docs
    Fixes #2

--- scalarmult.go
+++ scalarmult.go
@@ -4,7 +4,8 @@
 
 package edwards25519
 
-// ScalarBaseMult sets v = x * B, and returns v.
+// ScalarBaseMult sets v = x * B, where B is the canonical generator, and
+// returns v.
 //
 // The scalar multiplication is done in constant time.
 func (v *Point) ScalarBaseMult(x *Scalar) *Point {
@@ -146,7 +147,8 @@ func (v *Point) MultiScalarMult(scalars []*Scalar, points []*Point) *Point {
 	return v
 }
 
-// VarTimeDoubleScalarBaseMult sets v = a * A + b * B, and returns v.
+// VarTimeDoubleScalarBaseMult sets v = a * A + b * B, where B is the canonical
+// generator, and returns v.
 //
 // Execution time depends on the inputs.
 func (v *Point) VarTimeDoubleScalarBaseMult(a *Scalar, A *Point, b *Scalar) *Point {
